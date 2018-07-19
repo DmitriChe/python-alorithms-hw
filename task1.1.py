24ad8455117f7971a0c2d45e8efba2b1f5276524
DmitryChe <juneority@mail.ru>
2018-07-19 09:03:32 +0300

    удален символ ; в конце строки исправлено название переменной со snake-case на нижнее подчеркивание

--- task1.1.py
+++ task1.1.py
@@ -1,11 +1,11 @@
 # 1. Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь.
 # https://drive.google.com/file/d/1EKRgDPysjgvrihKp9si-Q9Fpcc2srlRf/view?usp=sharing
 
-strNum = input('Введите трёхзначное целое число: ');
+str_num = input('Введите трёхзначное целое число: ')
 
-a = int(strNum[0])
-b = int(strNum[1])
-c = int(strNum[2])
+a = int(str_num[0])
+b = int(str_num[1])
+c = int(str_num[2])
 
 summ = a + b + c
 mult = a * b * c
