e6e0ec0cd111ee138e08f619806938ee0c344520
DmitryChe <juneority@mail.ru>
2018-07-22 02:39:43 +0300

    Добавил ветку с файлами третьего Д.З.

---
+++ hw3/task3-5.py
@@ -0,0 +1,20 @@
+# 5. В массиве найти максимальный отрицательный элемент.
+# Вывести на экран его значение и позицию в массиве.
+import random
+
+arr = [random.randint(-100, 100) for _ in range(100)]
+
+max_negative = -101
+pos = -1
+
+for i in range(len(arr)):
+    if max_negative < arr[i] < 0:
+        max_negative = arr[i]
+        pos = i
+
+# вывод
+print(arr)
+if pos == -1:
+    print('Отрицательных чисел в массиве нет!')
+else:
+    print(f'Максимальное отрицательное число {max_negative} стоит на {pos}-й позиции')
